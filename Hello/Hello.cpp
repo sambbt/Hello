b8348038a6f298dfbaaf9ff08da644dc7b08024c
sam <sam@sam.com>
2011-11-09 14:29:05 -0500

    add Hello class

---
+++ Hello/Hello.cpp
@@ -0,0 +1,17 @@
+/*
+ * Hello.cpp
+ *
+ *  Created on: Nov 9, 2011
+ *      Author: sam
+ */
+
+#include "Hello.h"
+
+Hello::Hello() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Hello::~Hello() {
+	// TODO Auto-generated destructor stub
+}
---
+++ Hello/Hello.h
@@ -0,0 +1,17 @@
+/*
+ * Hello.h
+ *
+ *  Created on: Nov 9, 2011
+ *      Author: sam
+ */
+
+#ifndef HELLO_H_
+#define HELLO_H_
+
+class Hello {
+public:
+	Hello();
+	virtual ~Hello();
+};
+
+#endif /* HELLO_H_ */
